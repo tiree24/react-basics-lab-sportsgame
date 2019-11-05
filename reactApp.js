54ee509cf26065c3e6e13e970c5b5f81e93a3efe
Jackson <tireej@amazon.com>
2019-11-05 15:13:08 -0500

    update

--- reactApp.js
+++ reactApp.js
@@ -1,10 +1,54 @@
+class Team extends React.Component {
+
+  constructor(props) {
+    super(props)
+    this.state = {
+      shots: 0,
+      score: 0
+    }
+  }
+
+  shotHandler = () => {
+    this.setState((state, props) => ({
+      shots: state.shots + 1,
+
+    }))
+
+    if (Math.random() > 0.5) {
+      this.state.score++
+    }
+
+  }
+
+  render() {
+    return (<h1>
+      Yay! I'm the {this.props.name}
+      <img alt="team logo" width="100px" src={this.props.logo} />
+
+      shots: {this.state.shots}
+      score: {this.state.score}
+      <button onClick={this.shotHandler}>shoot</button>
+    </h1>)
+
+  }
+}
+
 
 // Deafault App component that all other compents are rendered through
-function App(props){
+function App(props) {
   return (
+   
     <div>
-      <h1>Welcome to the sports game starter</h1>
-      This file represents the code after completing the setup step in the lab instructions
+      
+       <div className="stats">
+
+      <Team name=" home Team" logo="Uzumaki_logo.png" />
+      <div className="versus">
+        <h1>VS</h1>
+      </div>
+      <Team name=" away Team" logo="uchiha_logo.jpg" />
+    
+      </div>
     </div>
   )
 }
