c448a069a2d3c51c9e7784fff731756339166a36
Jackson <tireej@amazon.com>
2019-11-07 16:15:34 -0500

    update

--- reactApp.js
+++ reactApp.js
@@ -6,9 +6,14 @@ class Team extends React.Component {
       shots: 0,
       score: 0
     }
+    this.sound = new Audio('Swish+2.mp3');
+    this.audio = new Audio('bball+crowd.mp3');
+    this.visible
   }
 
   shotHandler = () => {
+    // playsound
+    this.sound.play()
     this.setState((state, props) => ({
       shots: state.shots + 1,
 
@@ -16,38 +21,49 @@ class Team extends React.Component {
 
     if (Math.random() > 0.5) {
       this.state.score++
+      this.audio.play()
     }
 
   }
 
   render() {
+
     return (<h1>
       Yay! I'm the {this.props.name}
       <img alt="team logo" width="100px" src={this.props.logo} />
+      <div>
+        {this.state.shot / this.state.score}
+      </div>
+
 
       shots: {this.state.shots}
       score: {this.state.score}
       <button onClick={this.shotHandler}>shoot</button>
     </h1>)
 
-  }
+ 
+      }
+  
+
+
 }
 
 
 // Deafault App component that all other compents are rendered through
 function App(props) {
   return (
-   
-    <div>
-      
-       <div className="stats">
 
-      <Team name=" home Team" logo="Uzumaki_logo.png" />
-      <div className="versus">
-        <h1>VS</h1>
-      </div>
-      <Team name=" away Team" logo="uchiha_logo.jpg" />
-    
+    <div>
+
+      <div className="stats">
+
+        <Team name=" home Team" logo="Uzumaki_logo.png" />
+        <div className="versus">
+          <h2>VS</h2>
+        </div>
+
+        <Team name=" away Team" logo="uchiha_logo.jpg" />
+
       </div>
     </div>
   )
